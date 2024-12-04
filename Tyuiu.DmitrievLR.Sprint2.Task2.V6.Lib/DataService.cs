fcdc77bd3e7ca9d38b45b68dceed65184fcea05a
DmitrievLeonidR <Tyuiu.DmitrievLR2@outlook.com>
2024-12-04 17:40:09 +0500

    Task2

---
+++ Tyuiu.DmitrievLR.Sprint2.Task2.V6.Lib/DataService.cs
@@ -0,0 +1,22 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.DmitrievLR.Sprint2.Task2.V6.Lib
+{
+    public class DataService : ISprint2Task2V6
+    {
+        public bool CheckDotInShadedArea(int x, int y)
+        {
+            if ((x >= 3 && x <= 5 && y >= 3 && y <= 5) ||
+           (x >= 6 && x <= 10 && y >= 4 && y <= 7) ||
+           (x >= 8 && x <= 11 && y >= 9 && y <= 11) ||
+           (x >= 12 && x <= 15 && y >= 11 && y <= 13))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Tyuiu.DmitrievLR.Sprint2.Task2.V6/Program.cs
@@ -0,0 +1,46 @@
+﻿using Tyuiu.DmitrievLR.Sprint2.Task2.V6.Lib;
+namespace Tyuiu.DmitrievLR.Sprint2.Task2.V6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #2 | Выполнил: Дмитриев Леонид Романович | ИИПБ-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #6                                                            *");
+            Console.WriteLine("* Выполнил: Дмитриев Леонид Романович | ИИПБ-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("* Написать программу на, которая запрашивает целые значения с клавиатуры и вычисляет *");
+
+            Console.WriteLine("* находится ли точка с координатами X,Y в заштрихованной области.         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите координату X:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите координату Y:");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            var result = ds.CheckDotInShadedArea(x,y);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
