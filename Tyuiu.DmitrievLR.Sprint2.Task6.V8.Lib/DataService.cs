e9ea3ed52a9d89844f6d473ee13a54322382942a
DmitrievLeonidR <Tyuiu.DmitrievLR2@outlook.com>
2024-12-04 18:59:29 +0500

    Task6

---
+++ Tyuiu.DmitrievLR.Sprint2.Task6.V8.Lib/DataService.cs
@@ -0,0 +1,59 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.DmitrievLR.Sprint2.Task6.V8.Lib
+{
+    public class DataService : ISprint2Task6V8
+    {
+        public string FindDateOfPreviousDay(int m, int n)
+        {
+            int previousMonth = m;
+            int previousDay = n - 1;
+
+            switch (m)
+            {
+                case 1:
+                    previousMonth = 12;
+                    previousDay = 31;
+                    break;
+                case 2:
+                    previousMonth = 1;
+                    break;
+                case 3:
+                    previousMonth = 2;
+                    break;
+                case 4:
+                    previousMonth = 3;
+                    break;
+                case 5:
+                    previousMonth = 4;
+                    break;
+                case 6:
+                    previousMonth = 5;
+                    break;
+                case 7:
+                    previousMonth = 6;
+                    break;
+                case 8:
+                    previousMonth = 7;
+                    break;
+                case 9:
+                    previousMonth = 8;
+                    break;
+                case 10:
+                    previousMonth = 9;
+                    break;
+                case 11:
+                    previousMonth = 10;
+                    break;
+                case 12:
+                    previousMonth = 11;
+                    break;
+            }
+
+            string result = $"{previousDay}.{previousMonth+1}";
+
+            return result;
+
+        }
+    }
+}
---
+++ Tyuiu.DmitrievLR.Sprint2.Task6.V8/Program.cs
@@ -0,0 +1,47 @@
+﻿using Tyuiu.DmitrievLR.Sprint2.Task6.V8.Lib;
+namespace Tyuiu.DmitrievLR.Sprint2.Task6.V8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #2 | Выполнил: Дмитриев Леонид Романович | ИИПБ-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #8                                                            *");
+            Console.WriteLine("* Выполнил: Дмитриев Леонид Романович | ИИПБ-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("*                                                                         *");
+
+            Console.WriteLine("Введите порядковый номер месяца (1-12):");
+            int month = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите день месяца (1-31):");
+            int day = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            var result = ds.FindDateOfPreviousDay(month,day);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
